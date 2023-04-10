012e76db5cd21f0b032d036087816d0819efc6a5
vvenkatakri1 <vvenkatakri1@bloomberg.net>
2023-04-10 17:14:56 -0400

    dynamic port

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	http.HandleFunc("/", handler)
 	fmt.Println("Server listening on port 8080...")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":"+port, nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
